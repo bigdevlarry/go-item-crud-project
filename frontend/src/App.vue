0ba237665d17c166d8f3829f413d7b6796c97767
larry.sulebalogun <larry.sulebalogun@tillo.io>
2025-10-24 13:43:51 +0100

    added Frontend and docker setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-100">
+    <main class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <div class="px-4 py-6 sm:px-0">
+        <div class="bg-white rounded-lg shadow-md p-6">
+          <RouterView />
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
